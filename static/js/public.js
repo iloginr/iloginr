d69e04afed06fb5a235217e77aacf1ea4247d45e
Alin Voinea <alin@eaudeweb.ro>
2014-04-14 18:52:15 +0300

    Update to version 2.0

--- static/js/public.js
+++ static/js/public.js
@@ -13,18 +13,62 @@ iloginr.Box.prototype = {
     self.form = $('form', self.context);
     self.button = $('input[type=submit]', self.context);
     self.output = $('.results', self.context).hide();
+    self.advanced = self.context.find('#advanced');
+    self.appVersion = self.context.find('#appVersion');
+    self.caps = self.context.find('#caps');
+    self.numbers = self.context.find('#numbers');
+    self.special = self.context.find('#special');
+    self.pwdLength = self.context.find('#length');
+    self.pwdVersion = self.context.find('#version');
 
-    self.form.submit(function(){
-      self.submit();
+    self.advanced.click(function(){
+      return self.advancedOptions();
+    });
+
+    self.form.submit(function(evt){
+      evt.preventDefault();
+      if(self.appVersion.val() != '2'){
+        self.submit();
+      }else{
+        self.submit2();
+      }
       return false;
     });
   },
 
   submit: function(){
     var self = this;
-    var passwd = sha256_digest(self.service.val() + ":" + self.passwd.val());
+    var passwd = self.service.val() + ":" + self.passwd.val();
+    passwd = new jsSHA(passwd, "TEXT");
+    passwd = passwd.getHash("SHA-256", "HEX");
     passwd = passwd.slice(55, 64);
     self.output.html(passwd).show();
+  },
+
+  submit2: function(){
+    var self = this;
+    var version = parseInt(self.pwdVersion.val(), 10);
+    var passwd = self.service.val() + ':' + self.passwd.val() + ':' + version;
+    passwd = new jsSHA(passwd, "TEXT");
+    passwd = passwd.getHash("SHA-512", "HEX");
+    self.output.html(passwd).show();
+
+    var newpwd = [];
+    var length = parseInt(self.pwdLength.val(), 10);
+    for(var idx=0; idx<length; idx++){
+      var stop = length / 128;
+      var start = idx * stop;
+      var end  = idx * stop + stop;
+      var chunk = passwd.slice(start, end);
+      if(!chunk.length){
+        break;
+      }
+    }
+  },
+
+  advancedOptions: function(){
+    var self = this;
+    self.context.find('div.advanced').slideToggle();
   }
 };
 
